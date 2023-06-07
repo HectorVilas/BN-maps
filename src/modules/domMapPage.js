29b70a5b8947274d9cfd870019d11f7769917238
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-07 20:51:21 -0300

    create module for page, move import from main

---
+++ src/modules/domMapPage.js
@@ -0,0 +1,10 @@
+import mapData from './mapData';
+
+const testMap = mapData('./json/mapgen/house/2storymodern01.json');
+
+// eslint-disable-next-line no-console
+console.log(testMap);
+
+export default function domMapPage() {
+
+}
