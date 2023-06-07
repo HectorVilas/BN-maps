958766984b13a9c72cc3138e2279ccedfba1e3d3
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-06 23:03:06 -0300

    remove JSON imports, pass location as argument

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 import './src/styles/style.css';
 import mapData from './src/modules/mapData';
-// JSON files for testing
-// import bungalow from './src/json/mapgen/house/bungalow01.json';
-import modern from './src/json/mapgen/house/2storymodern01.json';
-// import gardenHouse from './src/json/mapgen/house/garden_house_1.json';
-// import house from './src/json/mapgen/house/house01.json';
-// import urban from './src/json/mapgen/house/urban_1_house.json';
 
-
-console.log(
-  mapData(modern)
-);
+mapData('./json/mapgen/house/2storymodern01.json');
