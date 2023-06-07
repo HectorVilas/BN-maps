378c1ed0c676b5a8dede929fd96c17a61ae3a768
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-06 23:01:16 -0300

    use fetch instead of import
    This way the JSON files will be read and stored on memory
    instead of being copied to the Js file.

--- src/modules/mapData.js
+++ src/modules/mapData.js
@@ -1,24 +1,27 @@
 // documentation: https://github.com/cataclysmbnteam/Cataclysm-BN/tree/upload/doc/JSON%20Mapping%20Guides
 
-export default function mapData(jsonMap) {
-  const map = {
-    width: jsonMap[0].object.rows[0].length,
-    height: jsonMap[0].object.rows.length,
+export default async function mapData(jsonMap) {
+  const fetchMap = await fetch(jsonMap);
+  const map = await fetchMap.json();
+  const organizedMap = {
+    width: map[0].object.rows[0].length,
+    height: map[0].object.rows.length,
     floors: [],
   };
 
   // list all om_terrain IDs
   const omTerrains = [];
-  jsonMap.forEach((floor) => {
+  map.forEach((floor) => {
     if (!omTerrains.includes(floor.om_terrain)) omTerrains.push(floor.om_terrain);
   });
   // create an empty array for each ID
-  omTerrains.forEach(() => map.floors.push([]));
+  omTerrains.forEach(() => organizedMap.floors.push([]));
   // group each floor variant
-  jsonMap.forEach((floor) => {
+  map.forEach((floor) => {
     const floorsIdx = omTerrains.indexOf(floor.om_terrain);
-    map.floors[floorsIdx].push(floor);
+    organizedMap.floors[floorsIdx].push(floor);
   });
 
-  return map;
+  console.log(organizedMap);
+  return organizedMap;
 };
