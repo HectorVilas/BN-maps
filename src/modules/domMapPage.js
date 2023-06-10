6011a1ecb817b5291c9833d8ce2b99c57285cd64
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-10 18:31:11 -0300

    add temporal input for floor and variant picker

--- src/modules/domMapPage.js
+++ src/modules/domMapPage.js
@@ -8,12 +8,27 @@ export default function domMapPage() {
   const btnTest = document.createElement('button');
   btnTest.textContent = 'load map';
   btnTest.addEventListener('click', () => {
-    drawMapInfo('./json/mapgen/house/2storymodern01.json', 0, 0);
+    const floor = document.querySelector('.floor');
+    const variant = document.querySelector('.variant');
+    drawMapInfo('./json/mapgen/house/2storymodern01.json', floor.value, variant.value);
+  });
+
+  const inputNums = [];
+  const numTest = ['floor', 'variant'];
+  numTest.forEach((inputNum) => {
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.min = 0;
+    input.max = 10;
+    input.value = 0;
+    input.classList.add(inputNum);
+    inputNums.push(input);
   });
 
   mapPage.append(
     mapViewer,
     btnTest,
+    ...inputNums,
   );
 
   return mapPage;
