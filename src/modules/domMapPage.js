79e56c770795e76e7e8089b8b8ee87e492f59c55
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-11 20:03:41 -0300

    remove btn, add listener to num imputs

--- src/modules/domMapPage.js
+++ src/modules/domMapPage.js
@@ -5,31 +5,30 @@ export default function domMapPage() {
   const mapViewer = document.createElement('div');
   mapViewer.setAttribute('id', 'map-viewer');
 
-  const btnTest = document.createElement('button');
-  btnTest.textContent = 'load map';
-  btnTest.addEventListener('click', () => {
-    const floor = document.querySelector('.floor');
-    const variant = document.querySelector('.variant');
-    drawMapInfo('./json/mapgen/house/2storymodern01.json', floor.value, variant.value);
-  });
-
   const inputNums = [];
   const numTest = ['floor', 'variant'];
   numTest.forEach((inputNum) => {
     const input = document.createElement('input');
     input.type = 'number';
     input.min = 0;
-    input.max = 10;
+    input.max = 3;
     input.value = 0;
     input.classList.add(inputNum);
+    input.addEventListener('change', () => {
+      const floor = document.querySelector('.floor');
+      const variant = document.querySelector('.variant');
+      drawMapInfo('./json/mapgen/house/2storymodern01.json', floor.value, variant.value);
+    });
     inputNums.push(input);
   });
 
   mapPage.append(
     mapViewer,
-    btnTest,
     ...inputNums,
   );
 
   return mapPage;
 }
+
+// show map on load
+drawMapInfo('./json/mapgen/house/2storymodern01.json', 0, 0);
