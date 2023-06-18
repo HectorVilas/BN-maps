545557f71f1bc5a33e4b197d1606a701c20d06d0
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-18 19:15:53 -0300

    remove imputs, return object instead of DOM manipulation

--- src/modules/domMapPage.js
+++ src/modules/domMapPage.js
@@ -1,31 +1,17 @@
+import mapData from './mapData';
 import drawMapInfo from './drawMapInfo';
 
-export default function domMapPage(jsonMap) {
+let loadedMap;
+
+export default async function domMapPage(jsonMap) {
+  loadedMap = await mapData(jsonMap);
+
   const mapPage = document.createElement('section');
   const mapViewer = document.createElement('div');
-  mapViewer.setAttribute('id', 'map-viewer');
+  mapViewer.id = 'map-viewer';
 
-  const inputNums = [];
-  const numTest = ['floor', 'variant'];
-  numTest.forEach((inputNum) => {
-    const input = document.createElement('input');
-    input.type = 'number';
-    input.min = 0;
-    input.max = 3;
-    input.value = 0;
-    input.classList.add(inputNum);
-    input.addEventListener('change', () => {
-      const floor = document.querySelector('.floor');
-      const variant = document.querySelector('.variant');
-      drawMapInfo(jsonMap, floor.value, variant.value);
-    });
-    inputNums.push(input);
-  });
-
-  mapPage.append(
-    mapViewer,
-    ...inputNums,
-  );
+  mapViewer.append(drawMapInfo(loadedMap, 0, 0));
+  mapPage.append(mapViewer);
 
   return mapPage;
 }
