8fa14a9e026161222cd94900628ec9c6b395835b
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-18 19:16:37 -0300

    refactor module, return object instead of append

--- src/modules/drawMapInfo.js
+++ src/modules/drawMapInfo.js
@@ -1,23 +1,20 @@
-import mapData from './mapData';
+export default function drawMapInfo(jsonMap, floor, variant) {
+  const generatedMap = document.createElement('div');
 
-export default async function drawMapInfo(jsonMap, floor, variant) {
-  const selectedMap = await mapData(jsonMap);
-  const mapViewer = document.querySelector('#map-viewer');
-  mapViewer.replaceChildren();
-
-  for (let y = 0; y < selectedMap.height; y += 1) {
+  for (let y = 0; y < jsonMap.height; y += 1) {
     const row = document.createElement('div');
     row.classList.add('row');
-    for (let x = 0; x < selectedMap.width; x += 1) {
+    for (let x = 0; x < jsonMap.width; x += 1) {
       const tile = document.createElement('div');
       tile.classList.add('tile');
       tile.dataset.x = x;
       tile.dataset.y = y;
-      tile.textContent = `${selectedMap.floors[floor][variant].object.rows[y][x]}`;
+      tile.textContent = `${jsonMap.floors[floor][variant].object.rows[y][x]}`;
 
       row.append(tile);
     }
-    mapViewer.append(row);
+    generatedMap.append(row);
   }
-  // console.log(selectedMap.floors[floor][variant]);
+
+  return generatedMap;
 }
