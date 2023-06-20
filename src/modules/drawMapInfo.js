2b906eb246e4dd6d9f651c157254d191a8290a70
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-20 17:48:36 -0300

    calculate map width and height directly from map
    The width and height of the chosen map will be calculated here instead
    of receiving those values from other module, allowing the drawing of
    different floors with different dimensions.

--- src/modules/drawMapInfo.js
+++ src/modules/drawMapInfo.js
@@ -1,15 +1,18 @@
 export default function drawMapInfo(jsonMap, floor, variant) {
   const generatedMap = document.createElement('div');
+  const chosenMap = jsonMap.floors[floor][variant];
+  const chosenMapWidth = chosenMap.object.rows[0].length;
+  const chosenMapHeight = chosenMap.object.rows.length;
 
-  for (let y = 0; y < jsonMap.height; y += 1) {
+  for (let y = 0; y < chosenMapHeight; y += 1) {
     const row = document.createElement('div');
     row.classList.add('row');
-    for (let x = 0; x < jsonMap.width; x += 1) {
+    for (let x = 0; x < chosenMapWidth; x += 1) {
       const tile = document.createElement('div');
       tile.classList.add('tile');
       tile.dataset.x = x;
       tile.dataset.y = y;
-      tile.textContent = `${jsonMap.floors[floor][variant].object.rows[y][x]}`;
+      tile.textContent = `${chosenMap.object.rows[y][x]}`;
 
       row.append(tile);
     }
