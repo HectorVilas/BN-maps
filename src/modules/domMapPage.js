49c4022f12c15f1a385d9537b39bd85cd970b02c
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-20 19:30:09 -0300

    add buttons for each floor and variants

--- src/modules/domMapPage.js
+++ src/modules/domMapPage.js
@@ -9,9 +9,35 @@ export default async function domMapPage(jsonMap) {
   const mapPage = document.createElement('section');
   const mapViewer = document.createElement('div');
   mapViewer.id = 'map-viewer';
+  const floorList = document.createElement('div');
+  floorList.id = 'map-floors';
+
+  loadedMap.floors.forEach((floor) => {
+    const div = document.createElement('div');
+
+    const btnFloor = document.createElement('button');
+    const omTerrain = floor[0].om_terrain;
+    btnFloor.textContent = `${
+      typeof omTerrain === 'string' ? omTerrain : omTerrain.toString().split(',').join(' + ')
+    }`;
+
+    div.append(btnFloor);
+    if (floor.length > 1) {
+      const btnVariantNext = document.createElement('button');
+      btnVariantNext.textContent = '>';
+      const btnVariantPrev = document.createElement('button');
+      btnVariantPrev.textContent = '<';
+      div.append(btnVariantNext, btnVariantPrev);
+    }
+
+    floorList.append(div);
+  });
 
   mapViewer.append(drawMapInfo(loadedMap, 0, 0));
-  mapPage.append(mapViewer);
+  mapPage.append(
+    mapViewer,
+    floorList,
+  );
 
   return mapPage;
 }
