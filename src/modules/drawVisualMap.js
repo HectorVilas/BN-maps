ab2e7eaff59972f4aa341811986918ba6584ae8b
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-06-26 21:32:32 -0300

    import getTiles, pass palettes and chars as arguments

--- src/modules/drawVisualMap.js
+++ src/modules/drawVisualMap.js
@@ -1,9 +1,35 @@
+import getTiles from './getTiles';
+
 export default async function drawVisualMap(jsonMap, floor, variant) {
   const generatedMap = document.createElement('div');
   const chosenMap = await jsonMap.floors[floor][variant];
-  const chosenMapWidth = chosenMap.object.rows[0].length;
-  const chosenMapHeight = chosenMap.object.rows.length;
+  const blueprint = chosenMap.object.rows;
+  const chosenMapWidth = blueprint[0].length;
+  const chosenMapHeight = blueprint.length;
+  const paletteList = {};
+  let charList = {};
 
+  // group all map palettes
+  if (chosenMap?.object?.terrain) paletteList.terrain = chosenMap.object.terrain;
+  if (chosenMap?.object?.furniture) paletteList.furniture = chosenMap.object.furniture;
+  if (chosenMap?.object?.palettes) paletteList.palettes = chosenMap.object.palettes;
+
+  // list all chars as keys, no values
+  blueprint.forEach((row) => {
+    row.split('').forEach((char) => {
+      charList[char] = {
+        terrain: undefined,
+        furniture: undefined,
+        items: undefined,
+        place_loot: undefined,
+      };
+    });
+  });
+
+  // add values with CSS background properties
+  charList = await getTiles('BrownLikeBears', charList, paletteList);
+
+  // create row divs and tile divs
   for (let y = 0; y < chosenMapHeight; y += 1) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -14,6 +40,7 @@ export default async function drawVisualMap(jsonMap, floor, variant) {
       tile.dataset.x = x;
       tile.dataset.y = y;
       tile.textContent = `${char}`;
+      tile.style.background = charList[char];
 
       row.append(tile);
     }
