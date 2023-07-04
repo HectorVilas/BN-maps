2f85ebdde3453a090578995f4d79a3777acf0fc3
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-07-04 14:16:53 -0300

    add events to list items

--- src/modules/createMapgenList.js
+++ src/modules/createMapgenList.js
@@ -1,17 +1,27 @@
+import domMapPage from './domMapPage';
+
 function listFolder(fileList, title) {
   const div = document.createElement('div');
   div.classList.add('mapgen-list-folder');
   const h2 = document.createElement('h2');
+  const ul = document.createElement('ul');
   h2.textContent = title;
-  div.append(h2);
+  div.append(h2, ul);
 
   for (const file in fileList) {
     if (Object.hasOwn(fileList, file)) {
       if (Array.isArray(fileList[file])) {
-        fileList[file].forEach((item) => {
-          const para = document.createElement('p');
-          para.textContent = item.split('/').at(-2).split('.json').at(0);
-          div.append(para);
+        fileList[file].forEach((item, i) => {
+          const li = document.createElement('li');
+          li.textContent = item.split('/').at(-2).split('.json').at(0);
+          li.title = fileList[file][i];
+
+          li.addEventListener('click', async () => {
+            const app = document.querySelector('#app');
+            app.replaceChildren(await domMapPage(li.title));
+          });
+
+          ul.append(li);
         });
       } else {
         div.append(listFolder(fileList[file], file));
